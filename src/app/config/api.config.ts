34d4d511fe8c5e8499aca4b504f107017f36d2d3
Mateus Silva Pereira <mateussilvapereira2018@gmail.com>
2023-08-05 18:49:47 -0400

    Update project

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-    baseUrl : 'http://localhost:8080'
+   // baseUrl : 'http://localhost:8080'https://helpdesk-backend-production-99e1.up.railway.app/
+   baseUrl :'https://helpdesk-backend-production-99e1.up.railway.app'
 }
